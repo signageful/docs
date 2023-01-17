5737204f27fdcb3c1f2a1386dbecfec27732495a
Julian Koehn <me@julian.pro>
2023-01-17 17:34:26 +0100

    feat: seo

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { DocsThemeConfig, useConfig } from 'nextra-theme-docs'
 import { Logo } from './components/Logo'
 import { useRouter } from 'next/router'
 
@@ -11,8 +11,35 @@ const config: DocsThemeConfig = {
     if (route !== '/') {
       return {
         titleTemplate: '%s – Signageful Docs',
+        
       }
     }
+  },
+  head: function useHead() {
+    const { title } = useConfig()
+    const { route } = useRouter()
+
+    return (
+        <>
+            <meta name="msapplication-TileColor" content="#fff" />
+            <meta name="theme-color" content="#fff" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta httpEquiv="Content-Language" content="de" />
+            <meta
+                name="description"
+                content="Signageful Dokumentation"
+            />
+            <meta
+                name="og:description"
+                content="Signageful Dokumentation"
+            />
+            <meta
+                name="og:title"
+                content={title ? title + ' – Signageful' : 'Signageful'}
+            />
+            <meta name="apple-mobile-web-app-title" content="Signageful" />
+        </>
+    )
   },
   editLink: {
     text: 'Bearbeite diese Seite auf GitHub →'
